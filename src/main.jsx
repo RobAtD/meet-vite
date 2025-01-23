dc97241677ac6e6dac9f776edbf326742edca6a6
Robert Ditler <r.ditler@devduck.de>
2025-01-23 13:05:40 +0100

    Adds Atatus integration

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from "atatus-spa";
+atatus.config("50444180d3514fd3abfe5494f3b6cda7").install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
