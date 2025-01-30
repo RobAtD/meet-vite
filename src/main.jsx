558f86dedb1d31e46e29f6fc885cad4650c233de
Robert Ditler <r.ditler@devduck.de>
2025-01-30 13:47:23 +0100

    Adds manifest, service worker and registration

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from "atatus-spa";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
+
 atatus.config("50444180d3514fd3abfe5494f3b6cda7").install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();
