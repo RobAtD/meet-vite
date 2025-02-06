b520ecf4198fcccf08308f83664ab28b34d5549d
Robert Ditler <r.ditler@devduck.de>
2025-02-06 10:01:34 +0100

    Changes margin of ScatterChart

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -21,7 +21,7 @@ const CityEventsChart = ({ allLocations, events }) => {
       const count = events.filter(
         (event) => event.location === location
       ).length;
-      const city = location.split(", ")[0];
+      const city = location.split(/, | - /)[0];
       return { city, count };
     });
     return data;
@@ -33,13 +33,25 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 80,
+          left: -30,
         }}
       >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{dx: 20, dy: 40, fontSize: 14}} />
-        <YAxis type="number" dataKey="count" name="Number of Events" allowDecimals={false} />
+        <XAxis
+          type="category"
+          dataKey="city"
+          name="City"
+          angle={60}
+          interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
+        <YAxis
+          type="number"
+          dataKey="count"
+          name="Number of Events"
+          allowDecimals={false}
+        />
         <Tooltip cursor={{ strokeDasharray: "3 3" }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
